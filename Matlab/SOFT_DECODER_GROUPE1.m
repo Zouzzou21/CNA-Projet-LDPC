427d9f67bc96be74dfe12c3b08f7546a74903f47
Leo BONNAIRE <leobonnaire@laposte.net>
2023-11-09 19:38:14 +0100

    started but stopped by team victory

--- Matlab/SOFT_DECODER_GROUPE1.m
+++ Matlab/SOFT_DECODER_GROUPE1.m
@@ -1,109 +1,31 @@
-% LDPC SOFT DECODER
-function corrected_code = SOFT_DECODER_GROUPE1(input_code_flip, parity_matrix, channel_probabilities, max_iterations)
-    % Initialize variables
-    received_code = input_code_flip'; % Transpose the input code for correct orientation
-    beliefs = channel_probabilities;
-    iterations = max_iterations;
+function c_cor = SOFT_DECODER_GROUPE1(c_ds_flip, H, P1_ds, MAX_ITER)
 
-    % Iterate through decoding process
-    for iter = 1:iterations
-        updated_beliefs = updateBeliefs(parity_matrix, beliefs);
-        beliefs = updateDecodedBits(received_code, updated_beliefs, beliefs);
-    end
-
-    % Make final decision on corrected code
-    corrected_bits = zeros(1, size(parity_matrix, 2));
-    for i = 1:length(beliefs)
-        corrected_bits(i) = (beliefs(i) > 0.5); % Thresholding to make the final decision
-    end
-
-    corrected_code = corrected_bits'; % Transpose back to the original orientation
-end
-
-% CHECK NODE (C NODE)
-% Function to update beliefs based on parity matrix
-function updated_beliefs = updateBeliefs(parity_matrix, beliefs)
-    updated_beliefs = zeros(1, size(parity_matrix, 2));
-
-    % Iterate through rows of the parity matrix
-    for row = 1:size(parity_matrix, 1)
-        selected_indices = find(parity_matrix(row, :) == 1);
-
-        if ~isempty(selected_indices)
-            selected_beliefs = beliefs(selected_indices);
-
-            % Update beliefs for each selected index
-            for i = 1:length(selected_beliefs)
-                target_beliefs = selected_beliefs;
-                target_beliefs(i) = [];
-                updated_beliefs(selected_indices(i)) = 1 - calculateParity(target_beliefs);
+    % [c_rows, c_cols] = size(c_ds_flip);
+    [h_rows, h_cols] = size(H);
+    
+    c_node_in = 1 * ones(h_rows, h_cols);
+    v_node_in = 1 * ones(h_rows, h_cols);
+    
+    % Initialise c_node_in with APP (a posteriori probabilities)
+    
+    for i = 1:h_rows
+        for j = 1:h_cols
+            if H(i, j)
+                c_node_in(i, j) = P1_ds(j);
             end
         end
     end
-end
-
-% VARIABLE NODE (V NODE)
-% Function to update decoded bits based on result beliefs
-function updated_decoded_bits = updateDecodedBits(received_code, result_beliefs, channel_beliefs)
-    updated_decoded_bits = channel_beliefs;
-
-    % Iterate through rows of the code matrix
-    for i = 1:size(received_code, 1)
-        selected_indices = find(received_code(i, :) == 1);
-
-        if ~isempty(selected_indices)
-            selected_result_beliefs = result_beliefs(selected_indices);
-            updateChannel(i, selected_result_beliefs, channel_beliefs);
+    
+    for iter = 1:MAX_ITER
+        for i = 1:h_rows
+            for j = 1:h_cols
+                % Compute the product of every row
+                total_products = prod(1-2*c_node_in, 2);
+                
+                % Divide the preceding product by the current value
+                c_node_out(i, j) = 0.5 + 0.5 * (total_products(i) / (1 - 2*c_node_in(i, j)));
+            end
         end
     end
-end
 
-% Function to calculate the parity value
-function result = calculateParity(target_beliefs)
-    product = 1;
-
-    % Multiply the factors for each belief
-    for i = 1:length(target_beliefs)
-        product = product * (1 - 2 * target_beliefs(i));
-    end
-
-    % Compute the final parity value
-    result = 0.5 + product / 2;
-end
-
-% Function to update channel beliefs
-function updateChannel(index, result_beliefs, channel_beliefs)
-    prob_1 = calculateProbabilityU(index, result_beliefs, channel_beliefs);
-    prob_0 = calculateProbabilityZ(index, result_beliefs, channel_beliefs);
-    total_prob = prob_1 + prob_0;
-    scaling_factor = 1 / total_prob;
-
-    % Update the channel beliefs based on parity values
-    channel_beliefs(index) = scaling_factor * prob_1;
-end
-
-% Function to calculate probability for Z
-function prob_Z = calculateProbabilityZ(index, result_beliefs, channel_beliefs)
-    product = 1;
-
-    % Multiply the factors for each result belief
-    for k = 1:length(result_beliefs)
-        product = product * result_beliefs(k);
-    end
-
-    % Compute the final probability for Z
-    prob_Z = (1 - channel_beliefs(index)) * product;
-end
-
-% Function to calculate probability for U
-function prob_U = calculateProbabilityU(index, result_beliefs, channel_beliefs)
-    product = 1;
-
-    % Multiply the factors for each result belief
-    for k = 1:length(result_beliefs)
-        product = product * result_beliefs(k);
-    end
-
-    % Compute the final probability for U
-    prob_U = channel_beliefs(index) * product;
 end
